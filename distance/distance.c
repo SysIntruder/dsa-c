a63d2c60256a88cc53c1dc91f22cc1deebdf8033
Raihan R <raihanryuichi@gmail.com>
2023-11-10 08:33:20 +0700

    refactor name

--- distance/distance.c
+++ distance/distance.c
@@ -1,69 +1,66 @@
 #include <stdio.h>
 #include <math.h>
 
-typedef struct point point_t;
-struct point {
+typedef struct coord coord_t;
+struct coord
+{
     double x;
     double y;
     double z;
 };
 
-void create_point(point_t*);
-double xyz_distance(point_t, point_t);
+void init_coord(coord_t*);
+double distance(coord_t, coord_t);
 
-int main(int argc, char const *argv[])
+int main(int argc, char const* argv[])
 {
-    point_t a;
-    point_t b;
+    coord_t a;
+    coord_t b;
 
     printf("Calculate 3D distance\n");
     printf("Input first coordinate:\n");
-    create_point(&a);
+    init_coord(&a);
     printf("First coordinate: %.1lf, %.1lf, %.1lf\n", a.x, a.y, a.z);
     printf("\n");
 
     printf("Input second coordinate:\n");
-    create_point(&b);
+    init_coord(&b);
     printf("Second coordinate: %.1lf, %.1lf, %.1lf\n", b.x, b.y, b.z);
     printf("\n");
-    
-    double dis, sqrt_dis;
 
-    dis = xyz_distance(a, b);
-    sqrt_dis = sqrt(dis);
+    double d = distance(a, b);
 
-    printf("\n");
-    printf("\u221A Distance is %.1lf:\n", dis);
-    printf("Distance is %.1lf:\n", sqrt_dis);
+    printf("\n\u221A Distance is %.1lf:\n", d);
+    printf("Distance is %.1lf:\n", sqrt(d));
 
     return 0;
 }
 
-void create_point(point_t* point)
+void init_coord(coord_t* coord)
 {
     printf("x1:");
-    scanf("%lf", &(point->x));
+    scanf("%lf", &(coord->x));
     printf("y1:");
-    scanf("%lf", &(point->y));
+    scanf("%lf", &(coord->y));
     printf("z1:");
-    scanf("%lf", &(point->z));
+    scanf("%lf", &(coord->z));
     printf("\n");
 
     return;
 }
 
-double xyz_distance(point_t a, point_t b)
+double distance(coord_t a, coord_t b)
 {
-    double xPow = pow(((b.x) - (a.x)), 2);
-    printf("X Diff Power: %.1lf\n", xPow);
+    double x = pow(((b.x) - (a.x)), 2);
+    printf("X Diff Power: %.1lf\n", x);
 
-    double yPow = pow(((b.y) - (a.y)), 2);
-    printf("Y Diff Power: %.1lf\n", yPow);
+    double y = pow(((b.y) - (a.y)), 2);
+    printf("Y Diff Power: %.1lf\n", y);
 
-    double zPow = pow(((b.z) - (a.z)), 2);
-    printf("Z Diff Power: %.1lf\n", zPow);
+    double z = pow(((b.z) - (a.z)), 2);
+    printf("Z Diff Power: %.1lf\n", z);
 
-    double d = xPow + yPow + zPow;
+    double d = x + y + z;
 
     return d;
 }
